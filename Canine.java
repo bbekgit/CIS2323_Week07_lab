d711e1149b61b61d7001c03453c7ad83d8e1af87
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-03 19:50:36 -0500

    +

---
+++ Canine.java
@@ -0,0 +1,16 @@
+public class Canine{
+	
+	protected int height;
+	protected boolean isDomesticated;
+	protected int Legs = 4;
+	
+	public void setHeight(int h){
+	    height = h;
+	}
+	
+	public boolean getDomesticated(){
+		return isDomesticated;
+	}
+	
+	
+}
